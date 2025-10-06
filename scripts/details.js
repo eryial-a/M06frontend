8f09b833c11ca990cf00f0b9d89ce2f2939424a2
eryial-a <kyletn22@gmail.com>
2025-10-06 10:02:14 -0500

    add delete function

--- scripts/details.js
+++ scripts/details.js
@@ -1,5 +1,5 @@
-addEventListener("DOMContentLoaded", async function(){
-    //document.querySelector("#deleteBtn").addEventListener("click", deleteSong)
+addEventListener("DOMContentLoaded", async function () {
+    document.querySelector("#deleteBtn").addEventListener("click", deleteSong)
     //grab search params from url following question mark
     const urlparam = new URLSearchParams(window.location.search)
     const songID = urlparam.get('id')
@@ -17,8 +17,22 @@ addEventListener("DOMContentLoaded", async function(){
     html += `
         <h3>Artist - ${song.artist} </h3>
         <p>Popularity - ${song.popularity} <p>
-        <p>Release Date - ${song.releaseDate.substring(0,10)} <p>
+        <p>Release Date - ${song.releaseDate.substring(0, 10)} <p>
     `
 
     document.querySelector("div").innerHTML = html
 })
+
+async function deleteSong() {
+    //currently crashes database after since it sends headers again but dont know how to fix
+    try {
+        const urlparam = new URLSearchParams(window.location.search)
+        const songID = urlparam.get('id')
+        document.location.href = "/";
+        const response = await fetch("http://localhost:3000/api/songs/" + songID, {
+            method: "DELETE"
+        })
+    } catch (err) {
+
+    }
+}
