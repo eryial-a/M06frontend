bdf55e61fa237ff9760ced48a06c84f9cf4b2492
eryial-a <kyletn22@gmail.com>
2025-10-06 10:59:13 -0500

    fix links

--- scripts/add_song.js
+++ scripts/add_song.js
@@ -14,7 +14,7 @@ async function addSong() {
         genre: document.querySelector('#genre').value ? document.querySelector("#genre").value.split(",") : []
     }
 
-    const response = await fetch("https://m06backend.onrender.com/api/songs",{
+    const response = await fetch("https://m06backend.onrender.com/api/songs/",{
         method: "POST",
         headers:{
             "Content-Type" : "application/json"
